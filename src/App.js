a019b29c25565ecac02620af326949dadf445465
DavidRinSE <davidrichardsonin@gmail.com>
2020-06-23 13:06:09 -0400

    working on new component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import Posts from './Posts'
 import './App.css';
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
   componentDidMount(){
     this.fetchPosts()
   }
   
- async  fetchPosts(){
-   const fetchData = await fetch('http://localhost:8000/posts/')
-   const fetchJSON = await fetchData.json()
-   console.log(fetchJSON)
- }
+  async  fetchPosts(){
+    const fetchData = await fetch('http://localhost:8000/posts/')
+    const fetchJSON = await fetchData.json()
+    this.setState({posts: fetchJSON})
+    console.log(this.state.posts)
+  }
 
   render() {
     return (
@@ -19,6 +27,7 @@ class App extends React.Component {
         <header>
           <h1>👻 Ghost Post</h1>
         </header>
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
